3f79a5b3c09d31b943acf1d1ed1c7294090ad147
Antonio Alexandre Neto <lima.aneto@yahoo.com>
2022-12-30 19:15:59 -0300

    adding some more exercices

---
+++ easy/1929-concatenation-of-array/solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public static void main(String [] args){
+
+        getConcatenation(new int[]{1,3,2,1});
+    }
+
+    public static int getConcatenation(int[] nums, int val) {
+        
+    }
+}
---
+++ easy/27-remove-element/solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public static void main(String [] args){
+
+        removeElement(new int[]{0,1,2,2,3,0,4,2}, 2);
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        
+    }
+}
